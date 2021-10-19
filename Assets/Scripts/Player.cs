238f53450199be4f4db7f43d6d33318020cbf761
07-Raqib <91867711+07-Raqib@users.noreply.github.com>
2021-10-19 15:39:05 +0800

    Added fly logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private Rigidbody rb;
+    public float upForce = 10f;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * upForce,ForceMode.Impulse);
+        }
     }
 }
